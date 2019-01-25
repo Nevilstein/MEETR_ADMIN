86237e510bc610983ce4e47c936619131cd9ce2f
Nevilstein <keikirishima891@gmail.com>
2019-01-25 16:18:37 +0800

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ActiveUsersComponent } from './active-users/active-users.component';
+import { MatchesComponent } from './matches/matches.component';
+import { AdminComponent } from './admin/admin.component';
+import { InactiveComponent } from './inactive/inactive.component';
+const routes: Routes = [
+	{
+		path: '', component: LoginComponent
+	},
+	{
+		path: 'dashboard', component: DashboardComponent
+	},
+	{
+		path: 'active', component: ActiveUsersComponent
+	},
+	{
+		path: 'matches', component: MatchesComponent
+	},
+	{
+		path: 'admin', component: AdminComponent
+	},
+	{
+		path: 'inactive', component: InactiveComponent
+	}
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ActiveUsersComponent } from './active-users/active-users.component';
+import { MatchesComponent } from './matches/matches.component';
+import { AdminComponent } from './admin/admin.component';
+import { InactiveComponent } from './inactive/inactive.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    ActiveUsersComponent,
+    MatchesComponent,
+    AdminComponent,
+    InactiveComponent
   ],
   imports: [
     BrowserModule,
